78c7dc5d208e36aa44b4c73bcb15c5c183d72b8a
Lihi79 <liki79ster@gmail.com>
2023-05-29 16:12:51 +0300

    lihi

--- 10-edit-screen/dist/controller.js
+++ 10-edit-screen/dist/controller.js
@@ -3,35 +3,49 @@ function handleAddItem(evt) {
     // console.log(evt);
     var blogTitle = evt.target.elements.blogTitle.value;
     var subTitle = evt.target.elements.subTitle.value;
-    // const username = evt.target.elements.username.value;
     var blogText = evt.target.elements.blogText.value;
-    // const date = evt.target.elements.date.value;
-    blog.push(new Blog(blogTitle, subTitle, blogText));
+    var imgUrl = evt.target.elements.imgUrl.value;
+    blog.push(new Blog(blogTitle, subTitle, blogText, imgUrl));
     // saveLocalStorage("data", blog);
     saveLocalStorage(blog);
     renderBlog(blog);
 }
+// user sign up
+function signUp(evt) {
+    var userName = evt.target.elements.userName.value;
+    var password = evt.target.elements.password.value;
+    var email = evt.target.elements.email.value;
+    users.push(new User(userName, password, email));
+}
+function saveUserLocalStorage(user) {
+    localStorage.setItem("user", JSON.stringify(user));
+}
+function getUserLocalStorage() {
+    var getLcPart1 = localStorage.getItem("user");
+    if (!getLcPart1)
+        throw new Error("get Local Storage is null");
+    var getLcPart2 = JSON.parse(getLcPart1);
+    return getLcPart2;
+}
+function signUpBtn(user) {
+    localStorage.setItem("user", JSON.stringify(user));
+    window.location.href = "../1-index.html";
+}
 // ------------------------------------------------------------
-// <p class="blog__user-name">${blogs.username}</p>
-// <p class="blog__date">${blogs.date}</p>
 function renderBlog(blog) {
     var html = blog
         .map(function (blogs) {
-        return "    \n      <div class=\"container\">\n        <div class=\"blog\">\n          <div class=\"blog__post\">\n            <div class=\"blog__text\">\n              <h1 class=\"blog__title\" id=\"title\">" + blogs.blogTitle + "</h1>\n              <h3 class=\"blog__subtitle\" id=\"subtitle\">" + blogs.subTitle + "</h3>\n\n              <div class=\"blog__name-date\">\n                <p class=\"blog__user-name\">--user name--</p>\n                <p class=\"blog__date\" id=\"date\">--date--</p>\n              </div>\n\n              <div class=\"blog__img\">\n                <img\n                  src=\" \"\n                  alt=\"\"\n                  id=\"upload-img\"\n                  width=\"700px\"\n                  height=\"500px\"\n                />\n              </div>\n\n              <p class=\"blog__p\">" + blogs.blogText + "</p>\n            </div>\n          </div>\n        </div>\n      </div>\n  </body>\n    ";
+        return "    \n      <div class=\"container\">\n        <div class=\"blog\">\n          <div class=\"blog__post\">\n            <div class=\"blog__text\">\n              <h1 class=\"blog__title\" id=\"title\">" + blogs.blogTitle + "</h1>\n              <h3 class=\"blog__subtitle\" id=\"subtitle\">" + blogs.subTitle + "</h3>\n\n              <div class=\"blog__name-date\">\n                <p class=\"blog__user-name\">--user name--</p>\n                <p class=\"blog__date\" id=\"date\">--date--</p>\n              </div>\n\n              <div class=\"blog__img\">\n                <img\n                  src=\"" + blogs.imgUrl + "\"\n                  alt=\"\"\n                  id=\"upload-img\"\n                />\n              </div>\n\n              <p class=\"blog__p\">" + blogs.blogText + "</p>\n            </div>\n          </div>\n        </div>\n      </div>\n  </body>\n    ";
     })
         .join(" ");
     // renderToScreen();
     // renderToScreen(blogRender, html);
     blogRender.innerHTML = html;
-    return html.;
+    return html;
 }
 function renderToScreen() {
     blogRender.innerHTML = renderBlog(blog);
-    // const reverseOrder = blog.map.reverseOrder;
 }
-// function renderToScreen(element: HTMLDivElement, date: string) {
-//   element.innerHTML = date;
-// }
 function saveLocalStorage(blog) {
     localStorage.setItem("blog", JSON.stringify(blog));
 }
@@ -69,12 +83,3 @@ function getLocalStorage() {
 //   return getLcPart2;
 // }
 // ---------------------------------------------------
-// function doneEdit(mealType: string) {
-//   console.log(textArea.value);
-//   localStorage.setItem(mealType, textArea.value);
-//   window.location.href = "1-index.html";
-//1. Read text from html element
-//2. Save to local storage
-//   localStorage.setItem("bf", text);
-//3. redirect to main page
-//   window.location.href = "/1-....html";
