3e322a9ea26461e00ab21444ff06627355bbbbe8
RachelFullStack <122094690+RachelFullStack@users.noreply.github.com>
2023-05-31 10:40:30 +0300

    update

---
+++ dist/validation.js
@@ -0,0 +1,22 @@
+var submitButton = document.querySelector(".btnField__singUp");
+submitButton.onclick = function () {
+    var userName = document.getElementById("userName").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var cpass = document.getElementById("cpass").value;
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("Email", email);
+    localStorage.setItem("Password", password);
+    localStorage.setItem("Cpassword", cpass);
+    if (userName == "" || email == "" || password == "" || cpass == "") {
+        alert("input field has no value");
+    }
+    else {
+        if (password !== cpass) {
+            alert("password not matching");
+        }
+        else {
+            alert("registration succeeded");
+        }
+    }
+};
---
+++ validation.ts
@@ -0,0 +1,22 @@
+const submitButton = document.querySelector(
+  ".btnField__singUp"
+) as HTMLButtonElement;
+submitButton.onclick = () => {
+  const userName = document.getElementById("userName").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const cpass = document.getElementById("cpass").value;
+  localStorage.setItem("userName", userName);
+  localStorage.setItem("Email", email);
+  localStorage.setItem("Password", password);
+  localStorage.setItem("Cpassword", cpass);
+  if (userName == "" || email == "" || password == "" || cpass == "") {
+    alert("input field has no value");
+  } else {
+    if (password !== cpass) {
+      alert("password not matching");
+    } else {
+      alert("registration succeeded");
+    }
+  }
+};
