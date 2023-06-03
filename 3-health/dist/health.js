26020775acc82df46efb432f09c444b0c2925cb9
Lihi79 <liki79ster@gmail.com>
2023-06-03 17:16:53 +0300

    lihi

--- 3-health/dist/health.js
+++
@@ -1,9 +0,0 @@
-// const button = document.querySelector("#button") as HTMLDListElement;
-// const option = document.querySelector("#dropdown") as HTMLDListElement;
-// button.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   handelHidden();
-// });
-// function handelHidden() {
-//   option.classList.toggle("hidden");
-// }
