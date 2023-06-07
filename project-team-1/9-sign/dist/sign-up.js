8fa9be2579d1f543487fbea11bd61e9f07250a26
RachelFullStack <122094690+RachelFullStack@users.noreply.github.com>
2023-06-07 08:30:26 +0300

    update

--- project-team-1/9-sign/dist/sign-up.js
+++ project-team-1/9-sign/dist/sign-up.js
@@ -1,76 +1,76 @@
-// model
-// --------
-var User = /** @class */ (function () {
-    function User(userName, email, password, cpass) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-        this.cpass = cpass;
-        this.uid = uid();
-    }
-    return User;
-}());
-var users = getUserLocalStorage();
-// helper
-// ---------
-function uid() {
-    return "id- " + Math.random();
-}
-// controller
-// ------------
-function comparePassword(password, comparePassword) {
-    var isPasswordValid = password == comparePassword ? true : false;
-    return isPasswordValid;
-}
-// view
-// --------
-var signUpButton = document.getElementById("signUp");
-// controller
-// ------------
-signUpButton === null || signUpButton === void 0 ? void 0 : signUpButton.addEventListener("click", function (event) {
-    signUp(event);
-});
-// controller
-// ------------
-function signUp(evt) {
-    evt.preventDefault();
-    var userName = document.getElementById("userName");
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    var confirmPassword = document.getElementById("cpass");
-    users.push(new User(userName.value, email.value, password.value, confirmPassword.value));
-    if (!comparePassword(password.value, confirmPassword.value)) {
-        alert("the 2 of the password need to be matched");
-        return;
-    }
-    if (isUserNameAvailable(userName.value)) {
-        saveUserLocalStorage(users);
-        window.location.href = "../2-login/2-login.html";
-        console.log(userName.value);
-    }
-    else {
-        alert("userName not Available");
-    }
-}
-function isUserNameAvailable(userName) {
-    var users = JSON.parse(localStorage.getItem("user") || "[]");
-    console.log(users);
-    var isUserNameAvailable = users.filter(function (user) { return user.userName == userName; }).length == 0;
-    console.log(users.filter(function (user) { return user.userName == userName; }));
-    return isUserNameAvailable;
-}
-// helper
-// ---------
-function saveUserLocalStorage(user) {
-    localStorage.setItem("user", JSON.stringify(user));
-}
-function getUserLocalStorage() {
-    return JSON.parse(localStorage.getItem("user") || "[]");
-}
-// controller
-// ------------
-function signUpBtn(user) {
-    console.log("hello");
-    localStorage.setItem("user", JSON.stringify(user));
-    window.location.href = "../1-index.html";
-}
+// // model
+// // --------
+// class User {
+//   uid: string;
+//   constructor(
+//     public userName: string,
+//     public email: string,
+//     public password: string,
+//     public cpass: string
+//   ) {
+//     this.uid = uid();
+//   }
+// }
+// const users: User[] = [];
+// // helper
+// // ---------
+// function uid(): string {
+//   return `id- ${Math.random()}`;
+// }
+// function isPasswordCompared(password, comparePassword) {
+//   const isPasswordValid = password == comparePassword ? true : false;
+//   return isPasswordValid;
+// }
+// // view
+// // --------
+// const signUpButton = document.getElementById("signUpBtn") as HTMLElement;
+// // controller
+// // ---------------
+// signUpButton?.addEventListener("click", (event) => {
+//   signUp(event);
+// });
+// function signUp(evt) {
+//   evt.preventDefault();
+//   const userName = document.getElementById("userName") as HTMLInputElement;
+//   const email = document.getElementById("email") as HTMLInputElement;
+//   const password = document.getElementById("password") as HTMLInputElement;
+//   const confirmPassword = document.getElementById("cpass") as HTMLInputElement;
+//   if (
+//     userName.value.length == 0 ||
+//     email.value.length == 0 ||
+//     confirmPassword.value.length == 0
+//   ) {
+//     alert("you need to fill all the fields!!!");
+//     return false;
+//   }
+//   users.push(
+//     new User(userName.value, email.value, password.value, confirmPassword.value)
+//   );
+//   if (!isPasswordCompared(password.value, confirmPassword.value)) {
+//     alert("the 2 of the password need to be matched");
+//     return;
+//   }
+//   if (isUserNameAvailable(userName.value)) {
+//     saveUserLocalStorage(users);
+//     window.location.href = "../2-login/2-login.html";
+//     console.log(userName.value);
+//   } else {
+//     alert("userName not Available");
+//   }
+// }
+// function isUserNameAvailable(userName) {
+//   const users = JSON.parse(localStorage.getItem("user") || "[]");
+//   const isUserNameAvailable = [];
+//   users.filter((user) => user.userName == userName).length == 0;
+//   return isUserNameAvailable;
+// }
+// function saveUserLocalStorage(user: User[]) {
+//   localStorage.setItem("user", JSON.stringify(user));
+// }
+// function getUserLocalStorage() {
+//   return JSON.parse(localStorage.getItem("user") || "[]");
+// }
+// function signUpBtn(user: User[]) {
+//   localStorage.setItem("user", JSON.stringify(user));
+//   window.location.href = "../1-index.html";
+// }
--- project-team-1/9-sign/sign-up.ts
+++ project-team-1/9-sign/sign-up.ts
@@ -1,99 +1,98 @@
-// model
-// --------
-class User {
-  uid: string;
-  constructor(
-    public userName: string,
-    public email: string,
-    public password: string,
-    public cpass: string
-  ) {
-    this.uid = uid();
-  }
-}
+// // model
+// // --------
+// class User {
+//   uid: string;
+//   constructor(
+//     public userName: string,
+//     public email: string,
+//     public password: string,
+//     public cpass: string
+//   ) {
+//     this.uid = uid();
+//   }
+// }
 
-const users: User[] = getUserLocalStorage();
+// const users: User[] = [];
 
-// helper
-// ---------
-function uid(): string {
-  return `id- ${Math.random()}`;
-}
+// // helper
+// // ---------
+// function uid(): string {
+//   return `id- ${Math.random()}`;
+// }
 
-// controller
-// ------------
-function comparePassword(password, comparePassword) {
-  const isPasswordValid = password == comparePassword ? true : false;
+// function isPasswordCompared(password, comparePassword) {
+//   const isPasswordValid = password == comparePassword ? true : false;
 
-  return isPasswordValid;
-}
+//   return isPasswordValid;
+// }
 
-// view
-// --------
-const signUpButton = document.getElementById("signUp") as HTMLElement;
+// // view
+// // --------
+// const signUpButton = document.getElementById("signUpBtn") as HTMLElement;
 
-// controller
-// ------------
-signUpButton?.addEventListener("click", (event) => {
-  signUp(event);
-});
+// // controller
+// // ---------------
+// signUpButton?.addEventListener("click", (event) => {
+//   signUp(event);
+// });
 
-// controller
-// ------------
-function signUp(evt) {
-  evt.preventDefault();
+// function signUp(evt) {
+//   evt.preventDefault();
 
-  const userName = document.getElementById("userName") as HTMLInputElement;
+//   const userName = document.getElementById("userName") as HTMLInputElement;
 
-  const email = document.getElementById("email") as HTMLInputElement;
+//   const email = document.getElementById("email") as HTMLInputElement;
 
-  const password = document.getElementById("password") as HTMLInputElement;
+//   const password = document.getElementById("password") as HTMLInputElement;
 
-  const confirmPassword = document.getElementById("cpass") as HTMLInputElement;
+//   const confirmPassword = document.getElementById("cpass") as HTMLInputElement;
 
-  users.push(
-    new User(userName.value, email.value, password.value, confirmPassword.value)
-  );
+//   if (
+//     userName.value.length == 0 ||
+//     email.value.length == 0 ||
+//     confirmPassword.value.length == 0
+//   ) {
+//     alert("you need to fill all the fields!!!");
+//     return false;
+//   }
 
-  if (!comparePassword(password.value, confirmPassword.value)) {
-    alert("the 2 of the password need to be matched");
-    return;
-  }
+//   users.push(
+//     new User(userName.value, email.value, password.value, confirmPassword.value)
+//   );
 
-  if (isUserNameAvailable(userName.value)) {
-    saveUserLocalStorage(users);
-    window.location.href = "../2-login/2-login.html";
-    console.log(userName.value);
-  } else {
-    alert("userName not Available");
-  }
-}
+//   if (!isPasswordCompared(password.value, confirmPassword.value)) {
+//     alert("the 2 of the password need to be matched");
+//     return;
+//   }
 
-function isUserNameAvailable(userName) {
-  const users = JSON.parse(localStorage.getItem("user") || "[]");
+//   if (isUserNameAvailable(userName.value)) {
+//     saveUserLocalStorage(users);
+//     window.location.href = "../2-login/2-login.html";
+//     console.log(userName.value);
+//   } else {
+//     alert("userName not Available");
+//   }
+// }
 
-  console.log(users);
-  const isUserNameAvailable =
-    users.filter((user) => user.userName == userName).length == 0;
-  console.log(users.filter((user) => user.userName == userName));
-  return isUserNameAvailable;
-}
+// function isUserNameAvailable(userName) {
+//   const users = JSON.parse(localStorage.getItem("user") || "[]");
 
-// helper
-// ---------
-function saveUserLocalStorage(user: User[]) {
-  localStorage.setItem("user", JSON.stringify(user));
-}
+//   const isUserNameAvailable = [];
 
-function getUserLocalStorage() {
-  return JSON.parse(localStorage.getItem("user") || "[]");
-}
+//   users.filter((user) => user.userName == userName).length == 0;
+//   return isUserNameAvailable;
+// }
 
-// controller
-// ------------
-function signUpBtn(user: User[]) {
-  console.log("hello");
+// function saveUserLocalStorage(user: User[]) {
+//   localStorage.setItem("user", JSON.stringify(user));
+// }
 
-  localStorage.setItem("user", JSON.stringify(user));
-  window.location.href = "../1-index.html";
-}
+// function getUserLocalStorage() {
+//   return JSON.parse(localStorage.getItem("user") || "[]");
+// }
+
+// function signUpBtn(user: User[]) {
+//   localStorage.setItem("user", JSON.stringify(user));
+
+//   window.location.href = "../1-index.html";
+// }
